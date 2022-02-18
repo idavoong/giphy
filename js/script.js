84e133aade572e452621727eaf731641c12bcf7e
Glitch (giphy-projec) <none>
2022-02-18 20:52:47 +0000

    🪙🐒 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -3,9 +3,28 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$(".search-button").click(function(){
-  
 
+
+$(".search-button").click(function() {
   
+ 
+  let userInput = $(".search-term").val();
+  let url = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&rating=pg&api_key=dc6zaTOxFJmzC";
+  
+  
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+
+    .then(function(data) {
+      console.log(data);
+      let randomVariable = Math.floor(Math.random() * data.data.length);
+      $(".introText").html(`<img src="${data.data[randomVariable].images.original.url}">`);
+      randomVariable = Math.floor(Math.random() * data.data.length);
+      $(".pictureOne").html(`<img src="${data.data[randomVariable].images.original.url}">`);
+      randomVariable = Math.floor(Math.random() * data.data.length);
+      $(".pictureTwo").html(`<img src="${data.data[randomVariable].images.original.url}">`);
+    });
+
 });
-
